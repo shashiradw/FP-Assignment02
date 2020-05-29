da3ee9adb0f94b1510c411f21127bee3726b4188
shashira <shashiradw7@gmail.com>
2020-05-29 15:27:46 +0000

    Assignment02 Uploaded

---
+++ 01.scala
@@ -0,0 +1,20 @@
+import scala.io.StdIn._
+
+object TakeHomeSalary extends App{
+        
+        def wage(normalHour:Int)=normalHour*150
+
+        def ot(otHour:Int)=otHour*75
+
+        def tax(income:Int)=income*0.1
+         
+        def income(normalHour:Int,otHour:Int)=wage(normalHour)+ot(otHour)
+        
+      
+        printf("Enter Normal Hours :")
+        var normalHour=readInt()
+        printf("Enter OT Hours :")
+        var otHour=readInt()
+
+        printf("Take home salary = %.2f \n",income(normalHour,otHour)-tax(income(normalHour,otHour))) 
+}
---
+++ 02.scala
@@ -0,0 +1,18 @@
+import scala.io.StdIn._
+
+object BestTicketPrice extends App{
+    
+  def attendee(price:Double)=120+(15-price)/5*20
+
+  def revenue(price:Double)=price*attendee(price)
+
+  def cost(price:Double)=500+3*attendee(price)
+
+  def profit(price:Double)=revenue(price)-cost(price)
+
+  printf("Enter the Ticket Price :")
+  var tprice=readDouble()
+  
+  printf("Profit for Given Ticket Price : %.2f \n ",profit(tprice))
+
+}
